17406ee65047adc862862578fde7aaea1c8d30c8
Vichea69 <smart.vichea@gmail.com>
2025-02-19 03:33:31 +0700

    Set up basic Express server

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('server is running on port 3000');
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+});
