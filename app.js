1c55ccf8d3a60f2464230c372da3b30b04cfb314
Vichea69 <smart.vichea@gmail.com>
2025-02-19 03:48:59 +0700

    Add welcome route and improve server startup logging

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000");
+app.get('/', (req, res) => {
+    res.send('Welcome to the Subcription Tracker-API!');
 });
+
+app.listen(3000, () => {
+    console.log('Subcription Tracker-API is running on http://localhost:3000');
+})
+
+export default app;
